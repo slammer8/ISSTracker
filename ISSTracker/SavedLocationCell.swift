b322e66d3dfdb7b8e8c042ccc6d6a96f4dbb785f
Sam Wu <Wu@Sams-MacBook-Air.local>
2016-07-14 18:27:45 -0400

    Adds ViewModel to SavedLocationCell

--- ISSTracker/SavedLocationCell.swift
+++ ISSTracker/SavedLocationCell.swift
@@ -7,9 +7,27 @@
 //
 
 import UIKit
+import CoreLocation
 
 final class SavedLocationCell: UITableViewCell {
 
+    /// Date format of the cell (September 15, 2016 3:45:25 pm PST)
+    private static let DateFormat = "MMMM dd yyyy h:mm:ss a zzz"
+
+    /// A view model to configure the cell
+    struct ViewModel {
+        var name: String?
+        let location: CLLocationCoordinate2D
+        let nextPass: NSDate
+    }
+    
+    /// When this is set, the view will be configured.
+    var viewModel: ViewModel? {
+        didSet{
+            setUpCell()
+        }
+    }
+    
     @IBOutlet private weak var favoriteNameStackView: UIStackView!
     @IBOutlet private weak var favoriteNameLabel: UILabel!
     @IBOutlet private weak var nextPassTimeLabel: UILabel!
@@ -21,4 +39,23 @@ final class SavedLocationCell: UITableViewCell {
         // Initialization code
     }
     
+    // MARK:- Private functions
+    
+    private func setUpCell() {
+        
+        if let name = viewModel?.name {
+            favoriteNameLabel.text = name
+            favoriteNameStackView.hidden = false
+        } else {
+            favoriteNameStackView.hidden = true
+        }
+        
+        nextPassTimeLabel.text = viewModel?.nextPass.convertToLocaleUsingDateFormat(self.dynamicType.DateFormat) ?? ""
+        
+        if let lat = viewModel?.location.latitude, lon = viewModel?.location.longitude {
+            coordinatesLabel.text = "\(lat), \(lon)"
+        } else {
+            coordinatesLabel.text = ""
+        }
+    }
 }
