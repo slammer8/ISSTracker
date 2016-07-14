e9e919a4d92f6bbd80a8e8a5bdcaa6371fdd719c
Sam Wu <Wu@Sams-MacBook-Air.local>
2016-07-14 17:46:47 -0400

    Adds Nibs and UI Setup

---
+++ ISSTracker/SaveLocationCell.swift
@@ -0,0 +1,18 @@
+//
+//  SaveLocationCell.swift
+//  ISSTracker
+//
+//  Created by Sam Wu on 7/14/16.
+//  Copyright © 2016 BothPoints. All rights reserved.
+//
+
+import UIKit
+
+final class SaveLocationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
---
+++ ISSTracker/SavedLocationCell.swift
@@ -0,0 +1,18 @@
+//
+//  SavedLocationCell.swift
+//  ISSTracker
+//
+//  Created by Sam Wu on 7/14/16.
+//  Copyright © 2016 BothPoints. All rights reserved.
+//
+
+import UIKit
+
+final class SavedLocationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
